690ff95e0b658e0bbd6dc03a0fa82e010a004907
Chandan Kumar <chandan.kumar01@outlook.com>
2020-10-18 08:12:59 +0530

    modified.sh

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,18 @@ docker build -t chadnankumar01/multi-client:latest -t chandankumar01/multi-clien
 docker build -t chadankumar01/multi-server:latest -t chandankumar01/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t chandankumar01/multi-worker:latest -t chandankumar01/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push chandankumar01/multi-client
-docker push chandankumar01/multi-server
-docker push chandankumar01/multi-worker
+docker push chandankumar01/multi-client:lstest
+docker push chandankumar01/multi-server:latest
+docker push chandankumar01/multi-worker:latest
+
+
+docker push chandankumar01/multi-client:$SHA
+docker push chandankumar01/multi-server:$SHA
+docker push chandankumar01/multi-worker:$SHA
 
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=chandankumar01/multi-server
+kubectl set image deployments/server-deployment server=chandankumar01/multi-server:$SHA
+kubectl set image deployments/client-deployment client=chandankumar01/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=chandankumar01/multi-worker:$SHA
+
